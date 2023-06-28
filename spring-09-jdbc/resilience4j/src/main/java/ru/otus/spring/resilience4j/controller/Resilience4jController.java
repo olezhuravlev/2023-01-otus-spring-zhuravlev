cd728536dd5e069732b4679609883323383a26e2
Oleg Zhuravlev <olezhuravlev@gmail.com>
2023-06-28 10:25:19 +0200

    Grafana dashboard added

--- spring-09-jdbc/resilience4j/src/main/java/ru/otus/spring/resilience4j/controller/Resilience4jController.java
+++ spring-09-jdbc/resilience4j/src/main/java/ru/otus/spring/resilience4j/controller/Resilience4jController.java
@@ -1,5 +1,10 @@
 package ru.otus.spring.resilience4j.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,8 +19,11 @@ public class Resilience4jController {
         this.apiCaller = apiCaller;
     }
 
-    // @CircuitBreaker(name = "homeApi")
-    // @Bulkhead(name = "homeApi")
+    @CircuitBreaker(name = "homeApi")
+    @RateLimiter(name = "homeApi")
+    @Bulkhead(name = "homeApi")
+    @Retry(name = "homeApi")
+    @TimeLimiter(name = "homeApi")
     @GetMapping(value = {"/"})
     @ResponseBody
     public String home() {
